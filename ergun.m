f1e2d2294ad772540094d087bc05380949f5c9c4
theodorechamp <theodore.champ@colorado.edu>
2019-10-19 19:36:57 -0600

    updated ergun eq. You're welcome.

--- ergun.m
+++ ergun.m
@@ -1,7 +1,9 @@
-function dPdV = ergun(T,P_t)
-mflow = 75000 % kmol/hr using a guess based on the Aspen simulation! 
-% Check against the new version
-D = 0.254 %ujust an initial guess!!!) units of meters, AKA 1" 
-Go = mflow/(pi*D^2/4);
+function dPdV = ergun(T,P_t,D,Lr)
+
+mflow = 75000; % kmol/hr using a guess based on the Aspen simulation! 
+D = 0.0254; %just an initial guess!!!) units of meters, AKA 1" 
+Dp = D/8; %Using heuristic Weimer gave in class on 10/10/19 that particle diameter should be 1/8 of tube diameter
+G0 = mflow/(pi*D^2/4);
 Ac = D * pi;
-dPdV = - 1/Ac(150*(1-eps)/Dp/G0*mu+7/4)*(1-eps)/eps^3/Dp/RhoGas*G0^2/Ar/101325;
+rho = ; %Density will be f(p,T) etc. Need to account for that here.
+dPdV = - 150/Ac*((1-phi)/(Dp*G0/(nu*rho))+(7/4))*((1-phi)/phi^3)*(G0^2/(rho*Dp));
