b68a22f2df914385ac6b27312fe36abf975f2096
theodorechamp <theodore.champ@colorado.edu>
2019-10-21 21:22:46 -0600

    Wrote EBal function, several values are dummy values for now

---
+++ energyBal.m
@@ -0,0 +1,21 @@
+function dTdV = energyBal(Hrxn,delCp,rates,Tlast,comp,Cp,dV)
+
+term1 = 0;
+
+for i = 1:Length(rates)
+    term1 = term1 + rates(i)*Htot(i); 
+end
+
+%Params
+U = 100; %Dummy Value
+D = .0245;
+Tinf = 298; %Dummy Value, 25C
+
+term2 = U * dV * 4 / D * (Tlast - Tinf);
+
+term3 = 0;
+for i = 1:Length(comp)
+    term3 = term3 + comp(i)*Cp(i);
+end
+
+dTdV = (term1 - term2) / term3;
