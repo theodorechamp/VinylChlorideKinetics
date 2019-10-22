624b75e88eebaaf1d34bc98e28f484e6776f4b60
theodorechamp <theodore.champ@colorado.edu>
2019-10-21 21:07:12 -0600

    Added main.m

---
+++ main.m
@@ -0,0 +1,15 @@
+% This file runs the script, ensuring the correct sequence of calling
+% functions
+
+
+
+%%%%%%%%%%%%%%
+% Parameters %
+%%%%%%%%%%%%%%
+Tin = 298 % not the official value 
+
+
+%%%%%%%%%
+% Logic %
+%%%%%%%%%
+
