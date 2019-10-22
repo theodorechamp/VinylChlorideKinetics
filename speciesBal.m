887281da5fb95a2bb5e4d79489adbe7f3dd00799
jade7579 <jade7579@JSCBA205-13.ad.colorado.edu>
2019-10-21 20:55:46 -0600

    Added the species balance function

---
+++ speciesBal.m
@@ -0,0 +1,21 @@
+function speciesBal = speciesBal(pp, rates)
+dpp1/dt = 
+dpp2/dt
+dpp3/dt
+dpp4/dt
+dpp5/dt
+dpp6/dt
+dpp7/dt
+dpp1/dt
+
+
+% 1 = c2h4
+% 2 = hcl
+% 3 = o2
+% 4 = 1,1,2-trichloroethane
+% 5 = co2
+% 6 = cl2
+% 7 = 1,2-dichloroethane
+% 8 = h2o
+
+end
