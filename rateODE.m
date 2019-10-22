82e7808ab361aa6dc07da4c2c4df1d613267ce85
theodorechamp <theodore.champ@colorado.edu>
2019-10-21 20:48:13 -0600

    rate -> rates

--- rateODE.m
+++ rateODE.m
@@ -1,11 +1,11 @@
-function rate = rateODE(pp, k) %pp is a vector of all partial pressures, k is a vector of all rate constants
+function rates = rateODE(pp, k) %pp is a vector of all partial pressures, k is a vector of all rate constants
 %This function solves the rate equations for all reactions in the vinyl
 %chloride process returning a 'rate' vector with an entry for each rate
 %expression
-rate(1) = k(1) * pp(1) * pp(6)^0.5;
-rate(2) = k(2) * pp(7) * pp(6)^0.5;
-rate(3) = k(3) * pp(1) * pp(3) * pp.(6)^0.5;
-rate(4) = k(4) * pp(3) / pp(6);
+rates(1) = k(1) * pp(1) * pp(6)^0.5;
+rates(2) = k(2) * pp(7) * pp(6)^0.5;
+rates(3) = k(3) * pp(1) * pp(3) * pp.(6)^0.5;
+rates(4) = k(4) * pp(3) / pp(6);
 
 % 1 = c2h4
 % 2 = hcl
