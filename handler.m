883cb2705172da05a4d61b2e16892ba50cc2e96f
desveauxj <jsd2498@gmail.com>
2019-10-23 23:03:01 -0600

    formatting and comments

--- handler.m
+++ handler.m
@@ -4,21 +4,26 @@ function outvar = handler(v, y, rates, phi)
 nflow = y(1);
 T = y(2);
 P_t = y(3);
-nflowTot = 0; %Check with Matt if mol flowrates or just mol comps should be used.
 
-for i = 1:Length(y(1)) %calculate total mole flow
+%Check with Matt if mol flowrates or just mol comps should be used.
+%calculate total mole flow
+nflowTot = 0; 
+for i = 1:Length(y(1)) 
    nflowTot = nflowTot + y(i);
 end
 
+%calculate mol fractions
 molFracs = [0,0,0,0,0,0,0,0];
-for i = 1:Length(molFracs) %calculate mol fractions
+for i = 1:Length(molFracs) 
     molFracs(i) = nflow(i)/nflowTot;
 end
 
-pp = [0,0,0,0,0,0,0,0];
-for i = 1:Length(pp) %calculate mol fractions
+%calculate partial pressures
+pp = [0,0,0,0,0,0,0,0]; 
+for i = 1:Length(pp) 
     pp(i) = molFracs(i) * P_t;
 end
+
 
 outvar(1) = rateODE(%pp, k);
 
