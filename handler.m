ecb52a87c91cfac7c13fd8628e75bb34dc53b07f
desveauxj <jsd2498@gmail.com>
2019-10-23 23:01:33 -0600

    Add some calc ish

--- handler.m
+++ handler.m
@@ -3,16 +3,26 @@ function outvar = handler(v, y, rates, phi)
 %unpacking y
 nflow = y(1);
 T = y(2);
-P = y(3);
-TotFlow = 0;
+P_t = y(3);
+nflowTot = 0; %Check with Matt if mol flowrates or just mol comps should be used.
 
-for i = 1:y(1)
-    
+for i = 1:Length(y(1)) %calculate total mole flow
+   nflowTot = nflowTot + y(i);
+end
+
+molFracs = [0,0,0,0,0,0,0,0];
+for i = 1:Length(molFracs) %calculate mol fractions
+    molFracs(i) = nflow(i)/nflowTot;
+end
+
+pp = [0,0,0,0,0,0,0,0];
+for i = 1:Length(pp) %calculate mol fractions
+    pp(i) = molFracs(i) * P_t;
 end
 
 outvar(1) = rateODE(%pp, k);
 
-outvar(2) = energyBal(Htot, rates, T, n_tot_flow, Cp, dV);
+outvar(2) = energyBal(Htot, rates, T, molFracs, Cp, dV);
 
 %calc pp, I
 
