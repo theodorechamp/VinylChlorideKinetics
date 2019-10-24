55475690a301e50ea0a646bbb9471845d2f51f88
desveauxj <jsd2498@gmail.com>
2019-10-23 19:20:28 -0600

    changed species bal to flow rates

--- speciesBal.m
+++ speciesBal.m
@@ -1,13 +1,13 @@
-function speciesBal = speciesBal(pp, rates, n_tot_flow, phi)
+function speciesBal = speciesBal(P, rates, n_tot_flow, phi)
 R = 
-dpp1dt = (-1*rates(1) - 1*rates(3)) * phi/n_tot_flow;
-dpp2dt = (-2*rates(1) - 1*rates(2) - 4*rates(4) + 4*rates(5)) * phi/n_tot_flow;
-dpp3dt = (-0.5*rates(2) - 3*rates(3) - rates(4) + rates(5)) * phi / n_tot_flow;
-dpp4dt = rates(2) * phi / n_tot_flow;
-dpp5dt = (2*rates(3)) * phi / n_tot_flow;
-dpp6dt = (2*rates(4) - 2*rates(5)) * phi / n_tot_flow;
-dpp7dt = (rates(1) - rates(2))(2*rates(3)) * phi / n_tot_flow;
-dpp8dt = (rates(1) + rates(2) + 2*rates(3) + 2*rates(4) - 2*rates(5)) * phi / n_tot_flow;
+dF1dVt = (-1*rates(1) - 1*rates(3)) * (1-phi);
+dF2dVt = (-2*rates(1) - 1*rates(2) + 4*rates(4)) * (1-phi);
+dF3dVt = (-0.5*rates(2) - 3*rates(3) + rates(4)) * (1-phi);
+dF4dVt = rates(2) * (1-phi);
+dF5dVt = (2*rates(3)) * (1-phi);
+dF6dVt = (2*rates(4)) * (1-phi);
+dF7dVt = (rates(1) - 1*rates(2)) * (1-phi);
+dF8dVt = (rates(1) + rates(2) + 2*rates(3) + 2*rates(4)) * (1-phi);
 
 
 % 1 = c2h4
