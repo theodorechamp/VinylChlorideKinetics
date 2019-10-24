e9975521efff4a91ce4f1951f74ba824ee87d8e5
desveauxj <jsd2498@gmail.com>
2019-10-24 12:17:46 -0600

    Changed ergun to calc G0 from MW; updated handler

--- ergun.m
+++ ergun.m
@@ -1,10 +1,14 @@
-function dPdV = ergun(T,P_t)
+function dPdV = ergun(T,P_t, MW, nflow)
 
-mflow = 75000; % kmol/hr using a guess based on the Aspen simulation
+mflowTot = 0; % mol/hr 
+for i = 1:Length(MW)
+    mflowTot = nflow(i)*MW(i);
+end
+
 Lr = 1; %Look into reactor length
 D = 0.0254; 
 Dp = D/8; %Using heuristic Weimer gave in class on 10/10/19 that particle diameter should be 1/8 of tube diameter
-G0 = mflow/(pi*D^2/4);
+G0 = mflowTot/(pi*D^2/4);
 Ac = D * pi;
 rho = 1; %Density will be f(p,T) etc. Need to account for that here.
 dPdV = - 150/Ac*((1-phi)/(Dp*G0/(nu*rho))+(7/4))*((1-phi)/phi^3)*(G0^2/(rho*Dp));
--- handler.m
+++ handler.m
@@ -1,9 +1,10 @@
-function outvar = handler(v, y, rates, phi)
+function outvar = handler(v, y, rates, phi, MW)
 
 %unpacking y
 nflow = y(1);
 T = y(2);
 P_t = y(3);
+
 
 %Check with Matt if mol flowrates or just mol comps should be used.
 %calculate total mole flow
@@ -31,6 +32,6 @@ outvar(2) = energyBal(Htot, rates, T, molFracs, Cp, dV);
 
 outvar(3) = speciesBal(rates, phi);
 
-outvar(4) = ergun(T,P_t);
+outvar(4) = ergun(T,P_t, MW, nflow);
 
 end
